2fe60f6c366a772bc2dcc44db81424fff4ba88da
Sean Cosentino <sparkcollaboration@gmail.com>
2016-08-18 03:10:43 -0400

    fixing issue in nav bar

--- db/schema.rb
+++ db/schema.rb
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(version: 20160617034839) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
+    t.string   "category"
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
     t.string   "description"
