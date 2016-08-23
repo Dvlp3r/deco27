cf580b1edf9ad42584f3d37a1cc75457fa242e38
ruslannastasiev <ruslan0607@gmail.com>
2016-08-24 00:52:52 +0300

    add setting for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,15 @@ Rails.application.configure do
     },
     url: ":s3_domain_url"
   }
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'deco27.herokuapp.com',
+    user_name:            ENV['EMAIL'],
+    password:             ENV['PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
