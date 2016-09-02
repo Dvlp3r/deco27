e5b440774d875645ff0fa278f2624acf4e79f789
ruslannastasiev <ruslan0607@gmail.com>
2016-09-02 17:59:51 +0300

    categories fix, galeery fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822141524) do
+ActiveRecord::Schema.define(version: 20160902120524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,7 +111,6 @@ ActiveRecord::Schema.define(version: 20160822141524) do
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
     t.string   "description"
-    t.integer  "category_id"
     t.string   "slug"
     t.boolean  "is_feature",       default: false
     t.string   "brand"
