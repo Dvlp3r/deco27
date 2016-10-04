43b996a1883941b4cfe44cf41aa1e6754b200997
ruslannastasiev <ruslan0607@gmail.com>
2016-10-05 01:13:08 +0300

    nested categories and products

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,9 @@
 module CategoriesHelper
+  def category_products_path(category)
+  	super.gsub('%2F', '/')
+  end
+
+  def category_product_path(category, product)
+  	super.gsub('%2F', '/')
+  end
 end
