f82f38cedf51cea7e0d966a0c85af50e7d23915d
zaindevloper <rzaingee@gmail.com>
2016-11-02 02:02:54 +0500

    add animation

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -3,3 +3,12 @@ $(document).ready(function(){
   	$('.search-form').toggleClass('hidden');
   })
 })
+
+
+
+    //paste this code under head tag or in a seperate js file.
+    // Wait for window load
+$(window).load(function() {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+});
