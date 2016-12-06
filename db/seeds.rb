ebd3c6aa6ffb381213226ac3b077e562b1009dc6
saddaqayyum57 <saddaqayyum@gmail.com>
2016-12-06 11:36:57 +0500

    Subcategories changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -59,33 +59,20 @@ Admin.create email: 'patrick@dvlper.com', password: '1234567890'
 # Category.find_or_create_by(name: "Stairs")
 
 categories = {}
-categories["Porcelains"] = ["Floors", "Living", "Dining", "Kitchen", "Walls", "Bathroom", "Patio",]
+categories["Porcelains"] = ["Concrete Look", "Stone Look", "Fabric Look", "Wood Look", "Mosaics"]
 
-categories["Walls"] = ["Wood", "Porcelain", "Leather", "Extravaganza",]
+categories["Walls"] = ["Finium", "Duchateau"]
 
-categories["Doors"] = ["Modern" => ["Wenge", "Matte White", "Gray", "Light Oak", "Mahogany"],
-                      "Transitional" => ["Wenge", "Matte White", "Gray", "Light Oak", "Mahogany"],
-                      "Eco" => ["Eco Wenge", "Eco White", "Eco Gray", "Eco Light Oak"],
-                      "Viva" => ["Rain of Hearts", "Fireworks", "Spring", "Streamers", "Spots", "Star", "Glass", "Two-Faced"],
-                      "Spazioquadro" => ["Sensazioni"],
-                      "Krona Frameless" => ["Swing Doors", "Sliding Systems"]]
+categories["Doors"] = ["Collections", "Moderns", "Transitional", "Eco"]
 
-categories["Bathroom"] = ["Catalog", "Furniture",
-                          "Water Sensations" => ["Square Shower Heads", "Rectangle Shower Heads", "Round Shower Heads", "Shower Controls", "Accessories"],
-                          "Bathroom Accessories" => ["Air", "Curve", "Form", "Tube", "Una"],
-                          "Faucet Collection" => ["Curve", "Form", "Spin", "Tube", "Una", "Wave"],
+categories["Bathroom"] = ["Bathroom Accessories" => ["Accessories"],
+                          "Faucet Collection" => ["Kitchen", "Bathroom"],
                           "Bathtub Collection" => ["ACM", "Acrylic"],
                           "Sinks" => ["Undermount", "ACM", "Ceramic"],
-                          "Plumbing Accessories" => ["Drains", "Bottle Trap", "Angle Valves", "Water Outlet", "Pop Ups", "Shower Arms/Sliding Bar", "Hose"],
                           "Toilet Collection" => ["Wall Hung", "One Piece"]
                           ]
 
-categories["Hardwood Floors"] = ["The Atelier Collection", "The Chateau Collection", "The Heritage Collection", "The Riverstone Collection",
-                                 "The Strata Collection", "The Terra Collection", "The Vintage Remains Collection", "The New Classics Collection",
-                                 "The Palais Collection",
-                                 "The Italian Collection" => ["Contermporary/Modern", "Casual/Country/Tropical", "Eclectic", "Commercial"]
-                                ]                              
-
+categories["Hardwood Floors"] = ["Studio 27", "The Planks", "Bois du Chandon"]
 categories["Decking"] = []
 
 categories.each do |k, v|
